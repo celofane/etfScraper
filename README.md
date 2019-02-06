594d981fad5187fc0f72e85bb222fbebdd5762c7
John Haskell <john.haskell@gmail.com>
2019-02-06 15:05:23 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# etfScraper
+ETF info data scraper
---
+++ scrape.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+
+const scrape = async () => {
+    const etfs = require('./vaneckETF.json');
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    let output = [];
+    for (const etf of etfs) {
+        const target = etf.target.replace('ZZZ', etf.ticker);
+        console.log(target);
+        await page.goto(target);
+        const res = await page.evaluate((etf) => {
+            let ret = { ticker:'', sharesOut: null, price: null, marketPrice: null };
+            try {
+                ret.ticker = etf.ticker;
+                ret.sharesOut = parseFloat(document.querySelector(etf.selSharesOut).innerHTML.replace(/,/g, ''));
+                ret.price = parseFloat(document.querySelector(etf.selPrice).innerHTML.replace('$', '').replace(/,/g, ''));
+                ret.marketPrice = parseFloat(document.querySelector(etf.selMarketPrice).innerHTML.split('<br>')[1].replace('$', '').replace(/"/g, '').replace(/,/g, ''));
+            }
+            catch (e) {
+                console.error(e);
+            }
+            return ret;
+        }, etf);
+        output.push(res);
+    }
+
+    console.log(output);
+    var fs = require('fs');
+    fs.writeFile('output.json', JSON.stringify(output), 'utf8', ()=> {});
+
+    await browser.close();
+}
+
+scrape();
